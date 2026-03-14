33cd38667e923ae0b8e7edf608f30f7644b92c3c
Eduardo Martino <dudumartino4@gmail.com>
2026-03-14 17:07:15 -0300

    Criação da classe Abrigo e implementação dela

---
+++ src/abrigo/Abrigo.java
@@ -0,0 +1,33 @@
+package abrigo;
+
+import java.util.ArrayList;
+
+public class Abrigo {
+
+    ArrayList<Animal> animais = new ArrayList<>();
+
+    public void adicionarAnimal(Animal animal) {
+        animais.add(animal);
+    }
+
+    public void listarAnimais() {
+
+        for (Animal a : animais) {
+            a.mostrarInfo();
+            System.out.println("----------------");
+        }
+    }
+
+        public void adotarAnimal(String nome) {
+
+            for (Animal a : animais) {
+
+                if (a.nome.equals(nome)) {
+                    a.statusAdocao = "Adotado";
+                    System.out.println(nome + " foi Adotado!");
+                    return;
+                }
+            }
+            System.out.println("Animal não encontrado.");
+        }
+    }
--- src/abrigo/Main.java
+++ src/abrigo/Main.java
@@ -3,15 +3,21 @@ package abrigo;
 public class Main {
     public static void main(String[] args) {
 
+        Abrigo abrigo = new Abrigo();
+
         Cachorro cachorro1 = new Cachorro("Rex", 3, "Disponível", "Labrador");
         Gato gato1 = new Gato("Mimi", 2, "Disponível", "Preto");
 
-            cachorro1.mostrarInfo();
-            /*cachorro1.latir();*/
+        abrigo.adicionarAnimal(cachorro1);
+        abrigo.adicionarAnimal(gato1);
 
-            System.out.println();
+        System.out.println("Animais no abrigo:");
+        abrigo.listarAnimais();
 
-            gato1.mostrarInfo();
-            /*gato1.miar();*/
+        System.out.println("\nAdotando Rex...\n");
+        abrigo.adotarAnimal("Rex");
+
+        System.out.println("\nLista atualizada:");
+        abrigo.listarAnimais();
     }
 }
